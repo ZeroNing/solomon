02141b0e598e824602c771a1920c654a9ff1b9f8
huangweihua <744302215@qq.com>
2022-03-16 17:44:50 +0800

    新增两个配置类用于获取jar包内国家化资源文件

--- solomon-parent/solomon-base/src/main/java/com/steven/solomon/config/i18n/I18nConfig.java
+++ solomon-parent/solomon-base/src/main/java/com/steven/solomon/config/i18n/I18nConfig.java
@@ -1,10 +1,13 @@
 package com.steven.solomon.config.i18n;
 
 import com.steven.solomon.base.code.BaseCode;
+import com.steven.solomon.i18n.I18nControl;
 import com.steven.solomon.utils.logger.LoggerUtils;
 import com.steven.solomon.utils.verification.ValidateUtils;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -26,17 +29,21 @@ public class I18nConfig {
    */
   @Bean("resourceBundleMessageSource")
   public ResourceBundleMessageSource init() {
+    ResourceBundle resources = ResourceBundle.getBundle("classpath*:i18n/messages", new I18nControl());
     ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
     bundleMessageSource.setDefaultEncoding(BaseCode.UTF8);
     if(ValidateUtils.isEmpty(BASE_NAMES)){
       logger.info("初始化I18N国际化文件缺少I18N文件路径，忽略初始化国际化配置");
       return bundleMessageSource;
     }
-    String[] baseNameList = ValidateUtils.isNotEmpty(BASE_NAMES) ? BASE_NAMES.split(",") : null;
-    bundleMessageSource.setBasenames(baseNameList);
+    String beanNames = BASE_NAMES;
+    beanNames = beanNames + "," + resources.getBaseBundleName();
+    List<String> baseNameList = Arrays.asList(beanNames.split(","));
+
+    bundleMessageSource.setBasenames(baseNameList.toString());
     bundleMessageSource.setDefaultLocale(ValidateUtils.isEmpty(DEFAULT_LOCALE) ? Locale.CHINESE : DEFAULT_LOCALE);
     bundleMessageSource.setDefaultEncoding("UTF-8");
-    logger.info("BaseI18nConfig初始化I18N国际化文件成功,国际化默认语言为:{},国际化文件路径为:{}",DEFAULT_LOCALE.toString(), Arrays.asList(baseNameList).toString());
+    logger.info("BaseI18nConfig初始化I18N国际化文件成功,国际化默认语言为:{},国际化文件路径为:{}",DEFAULT_LOCALE.toString(), baseNameList.toString());
     return bundleMessageSource;
   }
 
