c0fca620168f0a207ca9bd2f49caf7baac23f1da
huangweihua <744302215@qq.com>
2022-04-27 12:44:35 +0800

    feat() : 添加mongodb工具类

---
+++ solomon-parent/solomon-mongodb/src/main/java/com/steven/solomon/utils/MongoDbUtils.java
@@ -0,0 +1,100 @@
+package com.steven.solomon.utils;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.steven.solomon.enums.MongoDbRoleEnum;
+import com.steven.solomon.verification.ValidateUtils;
+import java.util.Collections;
+import org.bson.Document;
+
+public class MongoDbUtils {
+
+  /**
+   * 转换固定集合语句
+   * @param collectionName 集合名称
+   * @param max max则表示集合中文档的最大数量
+   * @param size 集合的大小，单位为kb
+   */
+  public static Document convertToCapped(String collectionName,Long max,Long size){
+    if(ValidateUtils.isEmpty(collectionName)){
+      return null;
+    }
+    Document doc = new Document();
+    doc.put("convertToCapped",collectionName);
+    if(ValidateUtils.isEmpty(size)){
+      doc.put("size",size);
+    }
+    if (ValidateUtils.isEmpty(max)) {
+      doc.put("max",max);
+    }
+    return doc;
+  }
+
+  /**
+   * 创建mongodb数据库以及集合（创建数据库不创建集合会导致mongdb数据库自动删除）
+   * @param mongoClient mongodb连接
+   * @param dbName 数据库名称
+   * @param collectionName 集合名词
+   * @return
+   */
+  public static MongoDatabase createDb(String mongoClient,String dbName,String collectionName){
+    if(ValidateUtils.isEmpty(mongoClient) || ValidateUtils.isEmpty(dbName) || ValidateUtils.isEmpty(collectionName)){
+      return null;
+    }
+    MongoDatabase mongoDatabase = createDb(mongoClient,dbName);
+    createCollection(mongoDatabase, dbName);
+    return mongoDatabase;
+  }
+
+  /**
+   * 创建用户并赋予权限
+   * @param mongoClient mongodb连接
+   * @param userName 用户名
+   * @param password 密码
+   * @param roleEnum mongodb权限
+   * @param dbName 数据库名（用户获取连接中的数据库名以及赋予数据库权限）
+   * @return
+   */
+  public static boolean createUser(String mongoClient,String userName,String password, MongoDbRoleEnum roleEnum,String dbName){
+    if(ValidateUtils.isEmpty(mongoClient) || ValidateUtils.isEmpty(dbName) || ValidateUtils.isEmpty(roleEnum) || ValidateUtils.isEmpty(dbName)){
+      return false;
+    }
+    try {
+      MongoClient mongoClients = MongoClients.create(mongoClient);
+
+      Document doc = new Document();
+      Document roleDoc = new Document();
+
+      roleDoc.put("role", roleEnum.getValue());
+      roleDoc.put("db", dbName);
+
+      doc.put("createUser",userName);
+      doc.put("pwd",password);
+      doc.put("roles", Collections.singletonList(roleDoc));
+      mongoClients.getDatabase(dbName).runCommand(doc);
+    }catch (Exception e) {
+      return false;
+    }
+    return true;
+  }
+
+  private static MongoDatabase createDb(String mongoClient, String dbName){
+    if(ValidateUtils.isEmpty(mongoClient) || ValidateUtils.isEmpty(dbName)){
+      return null;
+    }
+    MongoClient mongoClients = MongoClients.create(mongoClient);
+    MongoDatabase mongoDatabase = mongoClients.getDatabase(dbName);
+    return mongoDatabase;
+  }
+
+  private static void createCollection(MongoDatabase mongoDatabase,String collectionName){
+    if(ValidateUtils.isNotEmpty(mongoDatabase)){
+      MongoCollection<Document> mongoCollection =  mongoDatabase.getCollection(collectionName);
+      if(ValidateUtils.isEmpty(mongoCollection)){
+        mongoDatabase.createCollection(collectionName);
+      }
+    }
+  }
+}
