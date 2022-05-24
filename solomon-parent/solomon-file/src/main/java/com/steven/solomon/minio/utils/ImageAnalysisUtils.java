3f17d5962ab45891b4f1247a13329b4e8a407da9
huangweihua <744302215@qq.com>
2022-05-24 11:06:33 +0800

    增加图片读取exif信息

---
+++ solomon-parent/solomon-file/src/main/java/com/steven/solomon/minio/utils/ImageAnalysisUtils.java
@@ -0,0 +1,38 @@
+package com.steven.solomon.minio.utils;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 解析图片exif属性
+ */
+public class ImageAnalysisUtils {
+
+  public static Map<String,Object> analysis(String filePath) throws Exception {
+    return analysis(ImageMetadataReader.readMetadata(new File(filePath)));
+  }
+
+  public static Map<String,Object> analysis(InputStream inputStream) throws Exception {
+    return analysis(ImageMetadataReader.readMetadata(inputStream));
+  }
+
+  public static Map<String,Object> analysis(Metadata metadata) throws Exception {
+    Iterable<Directory> directories = metadata.getDirectories();
+    Map<String, Object> map = new HashMap<>();
+    for (Directory directory : directories) {
+      for (Tag tag : directory.getTags()) {
+        System.out.print(tag.getTagName() + " --> ");
+        System.out.println(tag.getDescription());
+        map.put(tag.getTagName(),tag.getDirectoryName());
+      }
+    }
+    return map;
+  }
+
+}
