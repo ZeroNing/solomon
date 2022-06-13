6db877d1f52e6357ef6185b5d03612e57463d2b4
huangweihua <744302215@qq.com>
2022-06-13 17:02:27 +0800

    调整mq发送消息方法

--- solomon-parent/solomon-mq/src/main/java/com/steven/solomon/utils/RabbitUtils.java
+++ solomon-parent/solomon-mq/src/main/java/com/steven/solomon/utils/RabbitUtils.java
@@ -2,57 +2,67 @@ package com.steven.solomon.utils;
 
 import com.steven.solomon.constant.code.BaseExceptionCode;
 import com.steven.solomon.exception.BaseException;
+import com.steven.solomon.init.RabbitMQInitConfig;
 import com.steven.solomon.logger.LoggerUtils;
 import com.steven.solomon.pojo.BaseMq;
 import com.steven.solomon.pojo.MessageQueueDatail;
 import com.steven.solomon.pojo.RabbitMqModel;
-import com.steven.solomon.init.RabbitMQInitConfig;
 import com.steven.solomon.verification.ValidateUtils;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
+import javax.annotation.Resource;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-@Service
-public class RabbitMqService implements MqService {
+@Component
+public class RabbitUtils {
 
-  private Logger logger = LoggerUtils.logger(RabbitMqService.class);
+  private Logger logger = LoggerUtils.logger(RabbitUtils.class);
 
-  @Autowired
+  @Resource
   private RabbitTemplate rabbitTemplate;
 
-  @Override
+  /**
+   * 发送消息
+   */
   public void send(BaseMq mq) throws Exception {
     if (!convertAndSend(mq,0,false)) {
       throw new BaseException(BaseExceptionCode.BASE_EXCEPTION_CODE);
     }
   }
 
-  @Override
+  /**
+   * 发送延缓信息
+   */
   public void sendDelay(BaseMq mq, long delay) throws Exception {
     if (!convertAndSend(mq,delay,true)) {
       throw new BaseException(BaseExceptionCode.BASE_EXCEPTION_CODE);
     }
   }
 
-  @Override
+  /**
+   * 发送消息,并设置消息过期时间
+   * @param mq
+   * @param expiration
+   * @throws Exception
+   */
   public void sendExpiration(BaseMq mq, long expiration) throws Exception {
     if (!convertAndSend(mq,expiration,false)) {
       throw new BaseException(BaseExceptionCode.BASE_EXCEPTION_CODE);
     }
   }
 
-  @Override
+  /**
+   * 重置队列并发使用者
+   */
   public boolean resetQueueConcurrentConsumers(String queueName, int concurrentConsumers) {
     Assert.state(concurrentConsumers > 0, "参数 'concurrentConsumers' 必须大于0.");
     DirectMessageListenerContainer container = (DirectMessageListenerContainer)findContainerByQueueName(queueName);
@@ -63,7 +73,9 @@ public class RabbitMqService implements MqService {
     return false;
   }
 
-  @Override
+  /**
+   * 重启消息监听者
+   */
   public boolean restartMessageListener(String queueName) {
     if(ValidateUtils.isEmpty(queueName)){
       logger.info("restartMessageListener 重启队列失败,传入队列名为空!");
@@ -74,7 +86,9 @@ public class RabbitMqService implements MqService {
     container.start();
     return true;
   }
-  @Override
+  /**
+   * 停止消息监听者
+   */
   public boolean stopMessageListener(String queueName) {
     if(ValidateUtils.isEmpty(queueName)){
       logger.info("restartMessageListener 停止队列失败,传入队列名为空!");
@@ -86,7 +100,7 @@ public class RabbitMqService implements MqService {
     return true;
   }
 
-//  private Map<String, AbstractMessageListenerContainer> getQueue2ContainerAllMap() {
+  //  private Map<String, AbstractMessageListenerContainer> getQueue2ContainerAllMap() {
 //    if (!hasInit) {
 //      synchronized (allQueueContainerMap) {
 //        if (!hasInit) {
@@ -123,11 +137,6 @@ public class RabbitMqService implements MqService {
         UUID.randomUUID().toString()));
     return true;
   }
-
-  /**
-   * queue2ContainerAllMap初始化标识
-   */
-//  private volatile boolean hasInit = false;
 
   public Collection<AbstractMessageListenerContainer> getAllQueueContainerList(){
     return RabbitMQInitConfig.allQueueContainerMap.values();
