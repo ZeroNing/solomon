5f6f4a043857677364803628dd1ab992dae2c0cc
huangweihua <744302215@qq.com>
2022-06-13 15:40:34 +0800

    调整rabbitmq发送失败以及丢失消失处理

--- solomon-parent/solomon-mq/src/main/java/com/steven/solomon/service/AbstractRabbitCallBack.java
+++ solomon-parent/solomon-mq/src/main/java/com/steven/solomon/service/AbstractRabbitCallBack.java
@@ -17,19 +17,4 @@ public abstract class AbstractRabbitCallBack {
    * 保存mq消息丢失方法
    */
   public abstract void saveReturnedMessage(ReturnedMessage returned);
-
-  public void confirm(CorrelationData correlationData, boolean ack, String cause){
-    if (!ack) {
-      logger.info("RabbitMQConfig:消息发送失败:correlationData({}),ack(false),cause({})", correlationData,cause);
-    } else {
-      logger.info("RabbitMQConfig:消息发送成功::correlationData({}),ack(true),cause({})", correlationData,cause);
-    }
-    this.saveRabbitCallBack(correlationData,ack,cause);
-  }
-
-  public void confirm(ReturnedMessage returned){
-    logger.info("RabbitMQConfig:消息丢失:exchange({}),route({}),replyCode({}),replyText({}),message:{}",
-            returned.getExchange(), returned.getRoutingKey(), returned.getReplyCode(), returned.getReplyText(), returned.getMessage());
-    this.saveReturnedMessage(returned);
-  }
 }
